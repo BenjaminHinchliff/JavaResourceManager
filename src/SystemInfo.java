ab4e52e2648f59eedd7cf0b4a0bd2fa5dd8f9870
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-03-28 14:11:45 -0700

    added basic classes

---
+++ src/SystemInfo.java
@@ -0,0 +1,4 @@
+public class SystemInfo {
+    // this class will encapsulate the C++ code that will get things like CPU Usage, etc. I should probably be the one
+    // to write the code for this one
+}
